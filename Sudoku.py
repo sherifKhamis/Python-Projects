14fd3143872206a74fb73e73798d6640c6ac614c
Sherif <sherifookhamis@gmail.com>
2023-04-02 17:37:07 +0200

    Started sudoku.py

---
+++ Sudoku.py
@@ -0,0 +1,22 @@
+# Sudoku Game in Python
+
+def menu():
+
+    print("Welcome to Sudoku in Python")
+    print("\nWhat do you want to do ?")
+    print("Start Game (a)")
+    print("View Highscores (b)")
+    print("Quit Program (c)")
+
+    choice = input("Choice: ")
+    if choice == "a":
+        print("Which difficulty do you want to play")
+        print("EASY (a)  MEDIUM (b)  HARD (c)")
+        difficulty = input("Choice: ")
+        # run_game()
+
+
+menu()
+
+
+
