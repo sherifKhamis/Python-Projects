7685755262be16c3f4791a97986a6d10a69ba9cf
Sherif <sherifookhamis@gmail.com>
2023-04-04 03:18:48 +0200

    Finished noRep() and generateGame()

--- Sudoku.py
+++ Sudoku.py
@@ -1,22 +1,42 @@
 # Sudoku Game in Python
 
-def menu():
-
-    print("Welcome to Sudoku in Python")
-    print("\nWhat do you want to do ?")
-    print("Start Game (a)")
-    print("View Highscores (b)")
-    print("Quit Program (c)")
-
-    choice = input("Choice: ")
-    if choice == "a":
-        print("Which difficulty do you want to play")
-        print("EASY (a)  MEDIUM (b)  HARD (c)")
-        difficulty = input("Choice: ")
-        # run_game()
+import numpy as np
+import random
 
 
-menu()
+def generateGame(difficulty):
+
+    global board
+    board = np.zeros((9, 9), dtype=int)
+    for rows in range(9):
+        for columns in range(9):
+            randomNumber = random.randrange(1, 10)
+            if random.randrange(difficulty) == 0:
+               board[rows][columns] = 0
+               continue
+            while not noRep(rows, columns, randomNumber):
+                randomNumber = random.randrange(1, 10)
+            board[rows][columns] = randomNumber
 
 
+def noRep(rows, columns, number):
 
+    # No repetition in rows
+    for column in board[rows]:
+        if column == number:
+            return False
+
+    # No repetition in columns
+    for x in range(9):
+        field = board[x][columns]
+        if field == number:
+            return False
+
+    # No repetition in square
+    for y in range(3):
+        for z in range(3):
+            if board[y+int(rows/3)*3][z+int(columns/3)*3] == number:
+                return False
+
+    # If no repetition found return True
+    return True
