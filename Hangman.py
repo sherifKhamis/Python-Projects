16838e8bbb4caf11d82b31b7bdcf82ecb4d54289
Sherif <sherifookhamis@gmail.com>
2023-04-13 02:10:38 +0200

    Added comments

--- Hangman.py
+++ Hangman.py
@@ -2,6 +2,7 @@
 
 import random
 
+# Possible words to be guessed
 words = ["HOSPITAL", "DOCTOR", "HOUSE", "TOWN", "VILLAGE", "AUTOMOTIVE", "MERCEDES", "DOG", "BATTERY"]
 word = random.choice(words)
 
@@ -69,29 +70,42 @@ print("Hangman Game in Python")
 
 
 def game():
+    # recursive function that runs the game
 
     global blanked_list
     global number_guesses
     global letters_guessed
     global index_list
 
+    # Print empty and already guessed fields
     for i in blanked_list:
         print(i, end=" ")
+
+    # Ask the user for a new letter
     new_letter = input("\nEnter the letter you want to try: ").upper()
+
+    # Check if new letter has already been guessed
     if new_letter in blanked_list:
         print("Letter already used! Try again.")
         game()
         return
+
+    # If new letter is part of to be guessed word and only occurs once, save it in blanked_list
     if word.find(new_letter) >= 0 and word.count(new_letter) == 1:
         index_in_word = word.index(new_letter)
         blanked_list[index_in_word] = new_letter
+
+    # If new letter is part of to be guessed word and occurs multiple times, save it in blanked_list
     elif word.count(new_letter) > 1:
         for letter in range(0, len(word)):
             if word[letter] == new_letter:
                 index_list += str(letter)
         for i in index_list:
             blanked_list[int(i)] = new_letter
+
+    # If new letter is NOT part of to be guessed word
     else:
+
         if new_letter in letters_guessed:
             print("Letter already used! Try again.")
             game()
