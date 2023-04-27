a36a0889a0abe6c0b9113daaf10c184db6bd7de7
Sherif <sherifookhamis@gmail.com>
2023-04-27 10:42:22 +0200

    Added saving highscores to a text database

--- Snake_Game/Scoreboard.py
+++ Snake_Game/Scoreboard.py
@@ -10,7 +10,11 @@ class Scoreboard(t):
         self.penup()
         self.hideturtle()
         self.goto(0, 380)
+        database = open("database.txt", "r+")
+        self.high_score = int(database.read())
+        database.close()
         self.refreshBoard()
+
 
     def increaseScore(self):
         self.score += 1
@@ -18,8 +22,17 @@ class Scoreboard(t):
         self.refreshBoard()
 
     def refreshBoard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.goto(-150, 375)
+        self.write(f"Score: {self.score}", font=("Arial", 20, "normal"))
+
+        self.goto(30, 375)
+        self.write(f"Highscore: {self.high_score}", font=("Arial", 20, "normal"))
 
     def gameOver(self):
+        if self.high_score < self.score:
+            database = open("database.txt", "w")
+            database.write(str(self.score))
+            database.close()
+
         self.goto(0, 0)
         self.write("GAME OVER", align="center", font=("Arial", 40, "normal"))
